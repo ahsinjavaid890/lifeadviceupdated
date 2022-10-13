94a2161b47e15514683ec1e4355c39b9bfc4bbd4
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2022-10-13 20:44:52 +0500

    Update homepage.blade.php

--- resources/views/admin/pages/homepage.blade.php
+++ resources/views/admin/pages/homepage.blade.php
@@ -7,51 +7,71 @@
         <!--begin::Container-->
         <div class="container-fluid">
             <!--begin::Card-->
-            <div class="card card-custom">
-                <div class="card-header flex-wrap border-0 pt-6 pb-0">
-                    <div class="card-title">
-                        <h3 class="card-label">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-header">
                             Homepage
                             <span class="text-muted pt-2 font-size-sm d-block">Manage All Section In Homepage</span>
-                        </h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Main Image</label>
+                                        <input type="file" style="height:45px;" class="form-control" name="">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Main Heading</label>
+                                        <input type="text" class="form-control" name="">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Sub Heading <small class="text-danger">Add Comma <b>,</b> for Seprator</small></label>
+                                        <input type="text" class="form-control" name="">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Button Text</label>
+                                        <input type="text" class="form-control" name="">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Button Link</label>
+                                        <input type="text" class="form-control" name="">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary" value="Update">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="card-body">
-                    <div class="tab-content">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Main Image</label>
-                                    <input type="file" style="height:45px;" class="form-control" name="">
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Company Logos
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Main Image</label>
+                                        <input type="file" style="height:45px;" class="form-control" name="">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Main Heading</label>
-                                    <input type="text" class="form-control" name="">
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Sub Heading <small class="text-danger">Add Comma <b>,</b> for Seprator</small></label>
-                                    <input type="text" class="form-control" name="">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Button Text</label>
-                                    <input type="text" class="form-control" name="">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Button Link</label>
-                                    <input type="text" class="form-control" name="">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input type="submit" class="btn btn-primary" value="Update">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary" value="Update">
+                                    </div>
                                 </div>
                             </div>
                         </div>
