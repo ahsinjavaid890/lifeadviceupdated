f675d24c42643f9107bacaa4b0caeef6d16a1622
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2022-11-05 16:45:04 +0500

    new changes

---
+++ app/Http/Controllers/Admin/Reportcontroller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Models\User;
+use App\Helpers\Cmf;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use App\Models\companies;
+use App\Models\wp_dh_companies;
+use App\Models\wp_dh_life_plans_benefits;
+use App\Models\help_articles;
+use App\Models\blogs;
+use App\Models\blogcategories;
+use App\Models\company_info_pages;
+use App\Models\wp_dh_insurance_plans_benefits;
+use App\Models\wp_dh_insurance_plans;
+use App\Models\wp_dh_life_plans;
+use App\Models\wp_dh_products;
+use App\Models\quotes;
+use Illuminate\Support\Facades\Hash;
+use Mail;
+use Auth;
+use DB;
+class Reportcontroller extends Controller
+{
+    public function salesreport()
+    {
+        return view('admin.reports.sales');
+    }
+}
---
+++ resources/views/admin/reports/sales.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.main-layout')
+@section('title','Sales Reports')
+@section('content')
+
+
+
+@endsection
