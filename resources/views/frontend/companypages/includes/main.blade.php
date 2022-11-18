dc0182ef639721c60f48d131d136406977a43a73
MalikAbubakar890 <96823154+MalikAbubakar890@users.noreply.github.com>
2022-11-18 21:31:55 +0500

    NEw changes

--- resources/views/frontend/companypages/includes/main.blade.php
+++ resources/views/frontend/companypages/includes/main.blade.php
@@ -13,6 +13,17 @@
 .health-inssurance-hero-banner {
     background-color: #262566;
 }
+.ageandcitizen input:focus-visible {
+    outline: none;
+    border-bottom: 1px solid #bbb !important;
+}
+.ageandcitizen input {
+    border: 0 !important;
+    border-bottom: 1px solid #bbb !important;
+    width: 100% !important;
+    height: 20px;
+    padding: 0px;
+}
 </style>
 @php
     $url = request()->segment(count(request()->segments()));
@@ -48,32 +59,82 @@
                 <div class="card date-card">
                     <div class="card-body">
                         <form method="post" action="#">
-                            <div class="container-homepage">
+                            <div class="container-fluid">
                             <div class="row">
-                                <div class="col-md-2">
-                                    <select class="form-control">
-                                        <option>Desination</option>
-                                        <option>Pakistan</option>
-                                        <option>India </option>
-                                        <option>Afghanistan</option>
-                                        <option>America</option>
-                                        <option>Dubai</option>
-                                    </select>
+                                <div class="col-md-4">
+                                    <select class="form-control" name="sum_insured2" id="sum_insured2">
+                                        <option value="">Coverage Amount</option>
+                                        <option value="100000">$100000</option>
+                                        <option value="150000">$150000</option>
+                                        <option value="200000">$200000</option>
+                                        <option value="250000">$250000</option>
+                                        <option value="300000">$300000</option>
+                                     </select>
+                                </div>
+                                <div class="col-md-4" style="margin-bottom: 10px;">
+                                    <input autocomplete="off" id="departure_date" name="departure_date" class="form-control" type="date" required="">
+                                      </div>
+                                <div class="col-md-4 ageandcitizen">
+                                    <div class="row yearsdiv" style="">
+                                       <div class="col-md-5">
+                                          <small style="font-size: 12px;color: #999;">Age</small>
+                                          <input type="text" name="ages[]" id="ages[]" value="" class="primaryage" style="margin-top: -5px !important;display: block;">
+                                       </div>
+                                       <div class="col-md-2 text-center text-dark" style="padding-top: 10px;">
+                                          or
+                                       </div>
+                                       <div class="col-md-5" style="padding-top: 10px;">
+                                          <a style="cursor: pointer; color: black;" onclick="$('.dobdiv').show(); $('.yearsdiv').hide()">Enter Date of Birth</a>
+                                       </div>
+                                    </div>
+                                     <div class="row dobdiv" style="display:none;">
+                                       <div class="col-md-12 d-flex">
+                                          <div class="col-md-6 no-padding d-flex">
+                                             <div class="col-md-4" style="padding: 0 5px;">
+                                                <small style="font-size: 10px;color: #999;padding: 0;">Month</small>
+                                                <input type="text" style="margin-top: -5px !important;display: block;">
+                                             </div>
+                                             <div class="col-md-4" style="padding: 0 5px;">
+                                                <small style="font-size: 10px;color: #999;padding: 0;">Day</small>
+                                                <input type="text" style="margin-top: -5px !important;display: block;">
+                                             </div>
+                                             <div class="col-md-4" style="padding: 0 5px;">
+                                                <small style="font-size: 10px;color: #999;padding: 0;">Year</small>
+                                                <input type="text" id="dob_year" onchange="calprimaryage()" style="margin-top: -5px !important;display: block;">
+                                             </div>
+                                          </div>
+                                          <script>
+                                             function calprimaryage(){
+                                             var currentyear = new Date().getFullYear();
+                                             var dobyear = document.getElementById('dob_year').value;
+                                             var primaryage = currentyear - dobyear;
+                                             $('.primaryage').val(primaryage);
+                                             }
+                                          </script>
+                                          <div class="col-md-6" style="padding-top: 10px;">
+                                             <div style="padding: 0;" class="col-md-2 text-center  text-dark">
+                                                or
+                                             </div>
+                                             <div style="padding: 0;text-align: center;" class="col-md-10">
+                                                <a style="cursor: pointer;  color: black;" onclick="$('.dobdiv').hide(); $('.yearsdiv').show()">Enter Age</a>
+                                             </div>
+                                          </div>
+                                       </div>
+                                    </div>
                                 </div>
                                 <div class="col-md-4">
-                                    <select class="form-control">
-                                        <option>Travel Information</option>
-                                        <option>Pakistan</option>
-                                        <option>India </option>
-                                        <option>Afghanistan</option>
-                                        <option>America</option>
-                                        <option>Dubai</option>
-                                    </select>
+                                    <div class="form-group">
+                                        <input type="email" name="email" class="form-control" placeholder="Enter Your Email">
+                                    </div>
                                 </div>
                                 <div class="col-md-4">
-                                    <div class="input-group mb-3">
-                                        <input type="text" class="form-control nice-select"id="fromDate" placeholder="Start Date">
-                                        <input type="text" id="toDate" class="form-control nice-select" placeholder="End Date" aria-label="Server">
+                                    <div class="form-group">
+                                        <select name="primary_destination" class="form-control" id="primary_destination" autocomplete="off" name="destination_country" required="">
+                                             <option value=""> --- Primary destination in Canada ---</option>
+                                             @foreach(DB::table('countries')->get() as $r)
+                                              <option value='{{ $r->name }}'  data-imagecss="flag {{ $r->data_imagecss }}" data-title="{{ $r->name }}">{{ $r->name }}</option>
+                                           @endforeach
+                                       </select>
                                     </div>
                                 </div>
                                 <div class="col-md-2">
