ab2af209250f72bfc1a70347c4165ea2b312d78e
MalikAbubakar890 <96823154+MalikAbubakar890@users.noreply.github.com>
2022-12-24 15:43:38 +0500

    New changes

--- resources/views/frontend/homepage/index.blade.php
+++ resources/views/frontend/homepage/index.blade.php
@@ -3,20 +3,6 @@
 <title>Life Insurance Ontario – Get Tips, Online Quotes for Life Insurance</title>
 @endsection
 @section('content')
-<style>
-    @media only screen and (min-width: 200px) and (max-width: 300px){
-  .mean-container a.meanmenu-reveal span {
-    top: 7px !important;
-    margin-top: -2px !important;
-}
-  }
-  @media only screen and (min-width: 768px) and (max-width: 1000px){
-.mean-container a.meanmenu-reveal span {
-    top: 7px !important;
-    margin-top: -2px !important;
-}
-  }
-</style>
 <div class="right-images">
  <img src="{{ asset('public/front/img/images/right-side.png') }}">
 </div>
@@ -352,7 +338,7 @@
 </section>
 <!-- Start Choose Us Area -->
 <section class="chooses-blogs choose-us-area-five pb-70" style="background-color:#f4f7fa;">
-   <div class="container-homepage blog-container">
+   <div class="container-homepage">
       <ul class="tabs row">
          @php
             $srno = 0;
@@ -784,170 +770,170 @@
 @endsection
 @section('script')
 <script type="text/javascript">
-	$('.clients').slick({
-	 dots: false,
-	  prevArrow: $('.prev'),
-	  nextArrow: $('.next'),
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 6,
-	  slidesToScroll: 6,
-	  responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	        slidesToShow: 5,
-	        slidesToScroll: 5,
-	        infinite: true,
-	        dots: false
-	      }
-	    },
-	    {
-	      breakpoint: 600,
-	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3
-	      }
-	    },
-	    {
-	      breakpoint: 480,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    }
-	  ]
-	});
-	var TxtRotate = function(el, toRotate, period) {
-	  this.toRotate = toRotate;
-	  this.el = el;
-	  this.loopNum = 0;
-	  this.period = parseInt(period, 10) || 2000;
-	  this.txt = '';
-	  this.tick();
-	  this.isDeleting = false;
-	};
+   $('.clients').slick({
+    dots: false,
+     prevArrow: $('.prev'),
+     nextArrow: $('.next'),
+     infinite: true,
+     speed: 300,
+     slidesToShow: 6,
+     slidesToScroll: 6,
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 5,
+           slidesToScroll: 5,
+           infinite: true,
+           dots: false
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 3
+         }
+       },
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+         }
+       }
+     ]
+   });
+   var TxtRotate = function(el, toRotate, period) {
+     this.toRotate = toRotate;
+     this.el = el;
+     this.loopNum = 0;
+     this.period = parseInt(period, 10) || 2000;
+     this.txt = '';
+     this.tick();
+     this.isDeleting = false;
+   };
 
-	TxtRotate.prototype.tick = function() {
-	  var i = this.loopNum % this.toRotate.length;
-	  var fullTxt = this.toRotate[i];
+   TxtRotate.prototype.tick = function() {
+     var i = this.loopNum % this.toRotate.length;
+     var fullTxt = this.toRotate[i];
 
-	  if (this.isDeleting) {
-	    this.txt = fullTxt.substring(0, this.txt.length - 1);
-	  } else {
-	    this.txt = fullTxt.substring(0, this.txt.length + 1);
-	  }
+     if (this.isDeleting) {
+       this.txt = fullTxt.substring(0, this.txt.length - 1);
+     } else {
+       this.txt = fullTxt.substring(0, this.txt.length + 1);
+     }
 
-	  this.el.innerHTML = '<span style="font-size:30px;" class="wrap">'+this.txt+'</span>';
+     this.el.innerHTML = '<span style="font-size:30px;" class="wrap">'+this.txt+'</span>';
 
-	  var that = this;
-	  var delta = 200 - Math.random() * 100;
+     var that = this;
+     var delta = 200 - Math.random() * 100;
 
-	  if (this.isDeleting) { delta /= 2; }
+     if (this.isDeleting) { delta /= 2; }
 
-	  if (!this.isDeleting && this.txt === fullTxt) {
-	    delta = this.period;
-	    this.isDeleting = true;
-	  } else if (this.isDeleting && this.txt === '') {
-	    this.isDeleting = false;
-	    this.loopNum++;
-	    delta = 500;
-	  }
+     if (!this.isDeleting && this.txt === fullTxt) {
+       delta = this.period;
+       this.isDeleting = true;
+     } else if (this.isDeleting && this.txt === '') {
+       this.isDeleting = false;
+       this.loopNum++;
+       delta = 500;
+     }
 
-	  setTimeout(function() {
-	    that.tick();
-	  }, delta);
-	};
+     setTimeout(function() {
+       that.tick();
+     }, delta);
+   };
 
-	window.onload = function() {
-	  var elements = document.getElementsByClassName('txt-rotate');
-	  for (var i=0; i<elements.length; i++) {
-	    var toRotate = elements[i].getAttribute('data-rotate');
-	    var period = elements[i].getAttribute('data-period');
-	    if (toRotate) {
-	      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-	    }
-	  }
-	  // INJECT CSS
-	  var css = document.createElement("style");
-	  css.type = "text/css";
-	  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
-	  document.body.appendChild(css);
-	};
-	$(document).ready(function(){
-	  $('.carousel').slick({
-	    speed: 500,
-	    slidesToShow: 4,
-	    slidesToScroll: 1,
-	    autoplay: true,
-	    autoplaySpeed: 2000,
-	    // dots:true,
-	    centerMode: true,
-	    responsive: [{
-	      breakpoint: 1024,
-	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 1,
-	        // centerMode: true,
+   window.onload = function() {
+     var elements = document.getElementsByClassName('txt-rotate');
+     for (var i=0; i<elements.length; i++) {
+       var toRotate = elements[i].getAttribute('data-rotate');
+       var period = elements[i].getAttribute('data-period');
+       if (toRotate) {
+         new TxtRotate(elements[i], JSON.parse(toRotate), period);
+       }
+     }
+     // INJECT CSS
+     var css = document.createElement("style");
+     css.type = "text/css";
+     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
+     document.body.appendChild(css);
+   };
+   $(document).ready(function(){
+     $('.carousel').slick({
+       speed: 500,
+       slidesToShow: 4,
+       slidesToScroll: 1,
+       autoplay: true,
+       autoplaySpeed: 2000,
+       // dots:true,
+       centerMode: true,
+       responsive: [{
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 1,
+           // centerMode: true,
 
-	      }
+         }
 
-	    }, {
-	      breakpoint: 800,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2,
-	        dots: true,
-	        infinite: true,
+       }, {
+         breakpoint: 800,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2,
+           dots: true,
+           infinite: true,
 
-	      }
-	    },  {
-	      breakpoint: 480,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1,
-	        dots: true,
-	        infinite: true,
-	        autoplay: true,
-	        autoplaySpeed: 2000,
-	      }
-	    }]
-	  });
-	});
-	function myFunction() {
-	var dots = document.getElementById("dots");
-	var moreText = document.getElementById("more");
-	var btnText = document.getElementById("myBtn");
+         }
+       },  {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           dots: true,
+           infinite: true,
+           autoplay: true,
+           autoplaySpeed: 2000,
+         }
+       }]
+     });
+   });
+   function myFunction() {
+   var dots = document.getElementById("dots");
+   var moreText = document.getElementById("more");
+   var btnText = document.getElementById("myBtn");
 
-	if (dots.style.display === "none") {
-	dots.style.display = "inline";
-	btnText.innerHTML = "Read more"; 
-	moreText.style.display = "none";
-	} else {
-	dots.style.display = "none";
-	btnText.innerHTML = "Read less"; 
-	moreText.style.display = "inline";
-	}
-	}
-	var $element=$('.each-event, .title');
-	var $window = $(window);
-	$window.on('scroll resize', check_for_fade);
-	$window.trigger('scroll');
-	function check_for_fade() { 
-	    var window_height = $window.height();
-	    
-	    $.each($element, function (event) {
-	        var $element = $(this);
-	        var element_height = $element.outerHeight();
-	        var element_offset = $element.offset().top;
-	        space = window_height - (element_height + element_offset -$(window).scrollTop());
-	        if (space < 60) {
-	            $element.addClass("non-focus");
-	        } else {
-	            $element.removeClass("non-focus");
-	        }
-	 
-	    });
-	};
+   if (dots.style.display === "none") {
+   dots.style.display = "inline";
+   btnText.innerHTML = "Read more"; 
+   moreText.style.display = "none";
+   } else {
+   dots.style.display = "none";
+   btnText.innerHTML = "Read less"; 
+   moreText.style.display = "inline";
+   }
+   }
+   var $element=$('.each-event, .title');
+   var $window = $(window);
+   $window.on('scroll resize', check_for_fade);
+   $window.trigger('scroll');
+   function check_for_fade() { 
+       var window_height = $window.height();
+       
+       $.each($element, function (event) {
+           var $element = $(this);
+           var element_height = $element.outerHeight();
+           var element_offset = $element.offset().top;
+           space = window_height - (element_height + element_offset -$(window).scrollTop());
+           if (space < 60) {
+               $element.addClass("non-focus");
+           } else {
+               $element.removeClass("non-focus");
+           }
+    
+       });
+   };
 </script>
 @endsection
