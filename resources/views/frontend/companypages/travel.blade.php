e8080fd8811676c528a1eb2fb49e58e87d566cc4
MalikAbubakar890 <96823154+MalikAbubakar890@users.noreply.github.com>
2023-01-05 16:15:02 +0500

    New cahnes

--- resources/views/frontend/companypages/travel.blade.php
+++ resources/views/frontend/companypages/travel.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.main')
 @include('frontend.companypages.includes.mettatittle')
 @section('content')
-@include('frontend.companypages.includes.main')
+@include('frontend.companypages.includes.maintravel')
 @include('frontend.companypages.includes.sectiontwo')
 @include('frontend.companypages.includes.sectionthree') 
 @include('frontend.companypages.includes.sectionfour')
