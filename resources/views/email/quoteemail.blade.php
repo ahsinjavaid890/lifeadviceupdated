49f37f5aac7ecc46ef34437eab4b66ce421b9ffc
MalikAbubakar890 <96823154+MalikAbubakar890@users.noreply.github.com>
2023-01-09 20:07:37 +0500

    New changes

--- resources/views/email/quoteemail.blade.php
+++ resources/views/email/quoteemail.blade.php
@@ -262,6 +262,7 @@
                                     </tr>
                                     <tr>
                                         <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+<<<<<<< Updated upstream
 
                                             <div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:24px;font-weight:bold;line-height:22px;text-align:center;color:white;">
                                                 Thank you for Purchasing Plan
@@ -283,9 +284,114 @@
                                 
 
                                 <h2 style="padding:20px;color: red;">Email is Under Construction</h2>
+=======
 
+                                            <div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:24px;font-weight:bold;line-height:22px;text-align:center;color:white;">
+                                                Thank you for Purchasing Plan
+                                            </div>
+>>>>>>> Stashed changes
 
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
 
+                                            <div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:14px;line-height:22px;text-align:left;">
+                                                <p>Hi,</p>
+                                                <p>We are Happy that You Purchased. In This Plan you will get Images to Upload. Duration of this plan is Life Time. </p>
+                                            </div>
+
+<<<<<<< Updated upstream
+=======
+                                        </td>
+                                    </tr>
+                                    <tr class="card-table" style="max-width: 100%; margin: auto; box-shadow: 0 4px 16px rgb(87 106 134 / 15%); border: 0; border-radius: 20px; border-left: 7px solid #262566;">
+                                    	<td class="body-table" style="-ms-flex: 1 1 auto; flex: 1 1 auto; min-height: 1px; padding: 1.25rem;">
+                                    		<table class="" style="display: -ms-flexbox; display: flex; -ms-flex-wrap: wrap; flex-wrap: wrap; margin-right: -15px; margin-left: -15px;">
+                                    			<tr>
+                                    				<td style="position: relative; width: 100%; padding-right: 100px; padding-left: 15px;">
+                                    					<div class="plan-label">
+																                  <h2>Essential Plan</h2>
+																              </div>
+																              <p class="plan-subheading" style="color: #67778f; font-size: 15px; font-weight: 600; line-height: 1.8;">Deductible Option ($1000 <span style="color:#2b3481;">(Included in premium)</span>)<br> Days: 365 (2023-1-21 - 2024-01-20)</p>
+																              <h2 class="qoute-policy" style="font-size: 21px; color: #67778f !important;">Policy Details</h2>
+																              <ul class="plan-highlights text-dark ml-3" style="list-style-image: url(data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0nMTInIGhlaWdodD0nMTAnIGZpbGw9J25vbmUnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZD0nTTguNzUuODIybC0uMDE4LjAyLS4wMTYuMDIzLTQuNDc4IDYuMDEtMS45ODMtMi4xMjdzMCAwIDAgMGEuOTk1Ljk5NSAwIDAwLTEuNDY2IDBoMGExLjA4NiAxLjA4NiAwIDAwMCAxLjQ3aDBsMi43NTkgMi45NnMwIDAgMCAwYy40LjQzIDEuMDY1LjQzIDEuNDY2IDBsLjAxNy0uMDIuMDE2LS4wMiA1LjE5LTYuODY4YTEuMDg3IDEuMDg3IDAgMDAtLjAyLTEuNDQ4Ljk5NS45OTUgMCAwMC0xLjQ2NiAweicgZmlsbD0nIzVFQTA0Nycgc3Ryb2tlPScjNUVBMDQ3Jy8+PC9zdmc+);">
+															                  <li><span>Quote Reference: <span class="plan-cat"> 1164635361</span></span></li>
+															                  <li><span>Quote Details : <span class="plan-cat">Super Visa Insurance</span></span></li>
+															                  <li><span>Total Premium: <span class="plan-cat">$1,235.16</span></span></li>
+															                  <li><span>Start Date: <span class="plan-cat">2023-1-10</span></span></li>
+															              </ul>
+                                    				</td>
+                                    				<td style="border-right: 1px solid #67778f;position: relative; width: 100%; padding-right: 80px; padding-left: 30px;">
+                                    						<div class="plan-coverage-limit" style="margin-top: 37px;">
+																                  <div class="limit-lable" style="margin-bottom: 1rem !important;">
+																                      <span style="color: #67778f; font-size: 20px;">Coverage Limit</span>
+																                  </div>
+																                  <div class="qoute-price-select">
+																                    <h2>$150,000</h2>
+																                  </div>
+																	              </div>
+																	              <div class="plan-coverage-limits" style="margin-top: 24px;">
+																                  <div class="limit-lable " style="margin-bottom: 1rem !important;">
+																                      <span style="color: #67778f; font-size: 20px;">Deductible</span>
+																                  </div>
+																                  <div class="qoute-price-select">
+																                    <h2>$1000 </h2>
+																                    </div>
+																              	</div>
+                                    				</td>
+                                    				<td style="position: relative; width: 100%; padding-right: 15px; padding-left: 53px;">
+                                    					<div class="compare compare-check " style="display: flex; text-align: right !important;">
+																	                <span class="" style="color: #67778f;">Compare</span>
+																	                <input style="height: 28px; width: 20px; margin-left: 10px; background-color: transparent; border-style: none; font: inherit;" type="checkbox" name="addtocompare" id="addtocompare" data-productid="23" data-pid="26" price="1235.16" value="1235.16" onclick="comparetest()">
+																	            </div>
+																	            <div class="qoute-logo" style="margin-top: 31px;">
+																                  <img style="max-width: 100%; height: auto;" src="http://localhost/lifeadvicelaravel/public/images/191020200954_TL.jpg">
+																              </div>
+																              <div class="total-price-traveller" style="margin-top: 21px; text-align: center;">
+																                  <h2 style="color: #262566; font-size: 1.5rem; line-height: 2rem; font-weight: 700;" id="traveler-price">$1,235.16<span style="color: #67778f; font-size: 11px; font-weight: 400; line-height: 24px; margin-left: 3px;">CAD</span></h2>
+																                  <p style="    margin-bottom: 0;"><span>1 Traveller(s)</span></p>
+																              </div>
+																              <div class="buy_now" style="background: #5ea047; color: white !important; border-radius: 33px;"> 
+																                <form method="POST" action="http://localhost/lifeadvicelaravel/apply">
+																                <input type="hidden" name="_token" value="ExX7jtHBzOas0lw0Dp1dzj7CEhCBRY39vk6SkedD">                <input type="hidden" value="abubakar19417malik@gmail.com" name="email">
+																                <input type="hidden" value="" name="fname">
+																                <input type="hidden" value="" name="lname">
+																                <input type="hidden" value="150000" name="coverage">
+																                <input type="hidden" value="1" name="traveller">
+																                <input type="hidden" value="1000" name="deductibles">
+																                <input type="hidden" value="20" name="deductible_rate">
+																                <input type="hidden" value="" name="person1">
+																                <input type="hidden" value="365" name="days">
+																                <input type="hidden" value="Travlance Insurance" name="companyName">
+																                <input type="hidden" value="2" name="comp_id">
+																                <input type="hidden" value="Essential Plan" name="planname">
+																                <input type="hidden" value="26" name="plan_id">
+																                <input type="hidden" value="2023-1-3" name="tripdate">
+																                <input type="hidden" value="2024-01-02" name="tripend">
+																                <input type="hidden" value="1235.16" name="premium">
+																                <input type="hidden" value="" name="destination">
+																                <input type="hidden" value="" name="cdestination">
+																                <input type="hidden" value="Super Visa Insurance" name="product_name">
+																                <input type="hidden" value="23" name="product_id">
+																                <input type="hidden" value="" name="country">
+																                <input type="hidden" value="Super Visa Insurance" name="visitor_visa_type">
+																                <input type="hidden" value="365" name="tripduration">
+																                <input type="hidden" value="49" name="age">
+																                <input type="hidden" value="-" name="dob">
+																                <input type="hidden" value="" name="agent">
+																                <input type="hidden" value="" name="broker">
+																                  <button class="btn btn-block text-white" type="submit">Buy</button>
+
+																              </form>
+																              </div>
+                                    				</td>
+                                    			</tr>
+                                    		</table>
+                                    	</td>
+                                    </tr>
+                                </table>
+>>>>>>> Stashed changes
                             </div>
                         </td>
                     </tr>
