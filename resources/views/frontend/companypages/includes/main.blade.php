c72f98d9eb03c8d06be785af30d9390f95fd0608
MalikAbubakar890 <96823154+MalikAbubakar890@users.noreply.github.com>
2023-01-20 16:50:40 +0500

    Update main.blade.php

--- resources/views/frontend/companypages/includes/main.blade.php
+++ resources/views/frontend/companypages/includes/main.blade.php
@@ -1402,24 +1402,16 @@ var dateInputMask = function dateInputMask(elm) {
     if(e.keyCode < 47 || e.keyCode > 57) {
       e.preventDefault();
     }
-    
-    var len = elm.value.length;
-    
-    // If we're at a particular place, let the user type the slash
-    // i.e., 12/12/1212
-    if(len !== 1 || len !== 3) {
+   var len = elm.value.length;
+   if(len !== 1 || len !== 3) {
       if(e.keyCode == 47) {
         e.preventDefault();
       }
     }
-    
-    // If they don't add the slash, do it for them...
-    if(len === 2) {
+   if(len === 2) {
       elm.value += '/';
     }
-
-    // If they don't add the slash, do it for them...
-    if(len === 5) {
+   if(len === 5) {
       elm.value += '/';
     }
   });
@@ -1433,29 +1425,22 @@ dateInputMask(input);
   
 var dateInputMasks = function dateInputMasks(elm) {
   elm.addEventListener('keypress', function(e) {
-    if(e.keyCode < 47 || e.keyCode > 57) {
+   if(e.keyCode < 47 || e.keyCode > 57) {
       e.preventDefault();
-    }
-    
-    var len = elm.value.length;
-    
-    // If we're at a particular place, let the user type the slash
-    // i.e., 12/12/1212
-    if(len !== 1 || len !== 3) {
+   }
+
+   var len = elm.value.length;
+   if(len !== 1 || len !== 3) {
       if(e.keyCode == 47) {
         e.preventDefault();
       }
-    }
-    
-    // If they don't add the slash, do it for them...
-    if(len === 2) {
+   }
+   if(len === 2) {
       elm.value += '/';
-    }
-
-    // If they don't add the slash, do it for them...
-    if(len === 5) {
+   }
+   if(len === 5) {
       elm.value += '/';
-    }
+   }
   });
 };
   
