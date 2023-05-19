b6e81c291a23fea6fac9fd609b0135221301c81b
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-05-19 18:35:37 +0500

    Update quoteemail.blade.php

--- resources/views/email/quoteemail.blade.php
+++ resources/views/email/quoteemail.blade.php
@@ -6,7 +6,6 @@ function object_to_array($object)
 {
    return (array) $object;
 }
-$buynowurl = $arrr[0]['url'];
 ?>
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
