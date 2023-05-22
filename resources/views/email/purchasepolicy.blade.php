50e76d629253200b59c7dac5253372ae09529585
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-05-22 16:33:25 +0500

    Update purchasepolicy.blade.php

--- resources/views/email/purchasepolicy.blade.php
+++ resources/views/email/purchasepolicy.blade.php
@@ -53,12 +53,12 @@
             margin: 13px 0;
         }
         .invoice tr th{
-            padding: 2px 13px 5px 0 !important;
-            border: 1px solid;
+            padding: 15px 15px 15px 15px !important;
+/*            border: 1px solid;*/
         }
         .invoice tr td{
             padding: 2px 13px 5px 5px;
-            border: 1px solid !important;
+/*            border: 1px solid !important;*/
         }
     </style>
     <style type="text/css">
@@ -79,7 +79,31 @@
         }
     </style>
 </head>
-
+@php
+    if($sale->product == 1){
+        $policytype = 'SVI';
+    } else if($sale->product == 2){
+        $policytype = 'VTC';
+    } else if($sale->product == 3){
+        $policytype = 'SI';
+    } else if($sale->product == 4){
+        $policytype = 'IFC';
+    } else if($sale->product == 5){
+        $policytype = 'ST';
+    } else if($sale->product == 6){
+        $policytype = 'MT';
+    } else if($sale->product == 7){
+        $policytype = 'AI';
+    } else if($sale->product == 8){
+        $policytype = 'TII';
+    } else if($sale->product == 9){
+        $policytype = 'BC';
+    }else{
+        $policytype = '';
+    }
+    $policy_number_temp = 10000000 + $sale->sales_id;
+    $policy_number = $policytype.$policy_number_temp;
+@endphp
 <body style="background-color:#f4f7fa;">
     <div style="background-color:#f4f7fa">
         <div style="background:#f9f9f9;background-color:#f9f9f9;Margin:0px auto;max-width:600px;">
@@ -94,31 +118,6 @@
             </table>
 
         </div>
-        @php
-            if($sale->product == 1){
-                $policytype = 'SVI';
-            } else if($sale->product == 2){
-                $policytype = 'VTC';
-            } else if($sale->product == 3){
-                $policytype = 'SI';
-            } else if($sale->product == 4){
-                $policytype = 'IFC';
-            } else if($sale->product == 5){
-                $policytype = 'ST';
-            } else if($sale->product == 6){
-                $policytype = 'MT';
-            } else if($sale->product == 7){
-                $policytype = 'AI';
-            } else if($sale->product == 8){
-                $policytype = 'TII';
-            } else if($sale->product == 9){
-                $policytype = 'BC';
-            }else{
-                $policytype = '';
-            }
-            $policy_number_temp = 10000000 + $sale->sales_id;
-            $policy_number = $policytype.$policy_number_temp;
-        @endphp
         <div style="background: #2b3481;background-color: #2b3481;Margin:0px auto;max-width:700px;">
 
             <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:white;background-color:white;width:100%;">
@@ -160,7 +159,7 @@
 
                                             <div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:14px;line-height:22px;text-align:left;color:#2b3481;">
                                                 <p>Hi {{ $request->name }},</p>
-                                                <p>We are Happy that You Purchased {{ $request->plan_name }}. </p>
+                                                <p>We truly appreciate your trust in our services and your decision to purchase an insurance policy with us. Your policy details and documentation will be emailed to you shortly. Please take the time to read them carefully and familiarize yourself with the terms and conditions. If you have any questions or need any assistance in understanding the specifics, don't hesitate to get in touch with us.</p>
                                             </div>
 
                                         </td>
@@ -168,53 +167,78 @@
                                     <tr>
                                         <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
 
-                                            <table 0="[object Object]" 1="[object Object]" 2="[object Object]" border="0" class="table table-bordered invoice" style="cellspacing:0;color:#2b3481;font-family:'Helvetica Neue',Arial,sans-serif;font-size:13px;line-height:22px;table-layout:auto;width:100%;">
+                                            <table 0="[object Object]" 1="[object Object]" 2="[object Object]" border="0" class="table table-bordered invoice" style="cellspacing:0;color:#2b3481;font-family:'Helvetica Neue',Arial,sans-serif;font-size:13px;line-height:22px;table-layout:auto;width:100%;background: #f9fafc;width: 100%;border: 1px solid #dae5ee;border-radius: 10px;">
                                                 <tr style="border-bottom:1px solid #ecedee;text-align:left;">
-                                                    <th style="padding: 0 15px 10px 0; border: 0;"><h3>Policy Details</h3> </th>
+                                                    <th style="padding: 0 15px 10px 0; border: 0;"><h3 style=" white-space: pre-wrap; color: #2b3481;  font-size: 20px; font-weight: 800; ">Policy Details</h3> </th>
                                                 </tr>
-                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
-                                                    
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Policy Number: </th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $policy_number }}</td>
+                                                    
+                                                </tr>
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Purchase Date:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->purchase_date }}</td>
                                                 </tr>
-                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
-                                                    <th style="padding: 0 15px 10px 0;">Policy Status:</th>
-                                                    <td style="padding: 5px 15px 5px 0;text-transform: capitalize;font-weight: bold;">{{ $sale->policy_status }}</td>
+                                                <tr style="text-align:left;">
+                                                    <th style="padding: 0 15px 10px 0;">Purchase Status:</th>
+                                                    <td style="padding: 5px 15px 5px 0;">{{ $sale->policy_status }}</td>
+                                                </tr>
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">End Date:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->end_date }}</td>
                                                 </tr>
-                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
-                                                    <th style="padding: 0 15px 10px 0;">Duration:</th>
-                                                    <td style="padding: 5px 15px 5px 0;">{{ $sale->duration }}</td>
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Total Price:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->price_total }}</td>
                                                 </tr>
-                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
+                                                <tr style="text-align:left;">
+                                                    <th style="padding: 0 15px 10px 0;">Duration:</th>
+                                                    <td style="padding: 5px 15px 5px 0;">{{ $sale->duration }}</td>
+                                                </tr>
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Coverage Amount:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $request->coverage }}</td>
+                                                </tr>
+                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
+                                                    
                                                     <th style="padding: 0 15px 10px 0;">Total Deductible:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $request->deductible_rate }}</td>
                                                 </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+
+                                            <table 0="[object Object]" 1="[object Object]" 2="[object Object]" border="0" class="table table-bordered invoice" style="cellspacing:0;color:#2b3481;font-family:'Helvetica Neue',Arial,sans-serif;font-size:13px;line-height:22px;table-layout:auto;width:100%;background: #f9fafc;width: 100%;border: 1px solid #dae5ee;border-radius: 10px;">
                                                 <tr style="border-bottom:1px solid #ecedee;text-align:left;">
-                                                    <th style="padding: 0 15px 10px 0; border: 0"><h3>Primary Insured Details:</h3> </th>
+                                                    <th style="padding: 0 15px 10px 0; border: 0;"><h3 style=" white-space: pre-wrap; color: #2b3481;  font-size: 20px; font-weight: 800; ">Primary Insured Details</h3> </th>
                                                 </tr>
-                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Name:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->fname }} {{ $sale->lname }}</td>
+                                                    
+                                                </tr>
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Age/DOB:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->dob }}</td>
                                                 </tr>
-                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Email:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->email }}</td>
+                                                    
+                                                </tr>
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Phone:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->phone }}</td>
                                                 </tr>
-                                                <tr style="border-bottom:1px solid #ecedee;text-align:left;">
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Address:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->address }}</td>
+                                                    
+                                                </tr>
+                                                <tr style="text-align:left;">
                                                     <th style="padding: 0 15px 10px 0;">Country:</th>
                                                     <td style="padding: 5px 15px 5px 0;">{{ $sale->country }}</td>
                                                 </tr>
@@ -223,8 +247,7 @@
                                     </tr>
                                     <tr>
                                         <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                                            <div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:14px;line-height:20px;text-align:left;color:#2b3481;">
-                                                Best regards,<br><br> Csaba Kissi<br>Elerion ltd., CEO and Founder
+                                            <div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:14px;line-height:20px;text-align:left;color:#2b3481;">We look forward to a long and rewarding relationship with you. Thank you once again for trusting Life Advice Insurance Inc. We're here to support you every step of the way.<br>Best Regards,
                                                 <br>
                                                 <a href="{{ url('') }}" style="color:#2F67F6">{{ url('') }}</a>
                                             </div>
