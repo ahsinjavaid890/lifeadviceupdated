e49485a8e5aa72478780c19fceb70ba8a5775e99
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-07-22 17:48:51 +0500

    Update serverinfo.blade.php

--- resources/views/admin/website/serverinfo.blade.php
+++ resources/views/admin/website/serverinfo.blade.php
@@ -16,19 +16,11 @@
                                 <div class="table-responsive">
                                     <ul class="list-group">
                                         @foreach($_SERVER as $key=>$r)
-                                        @if($key == 'HTTP_COOKIE')
-
-                                        @else
-
-                                        @if($key == 'PATH')
-
-                                        @else
                                         <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <span>{{$key}}</span>
+                                            <span>{{ $key }}</span>
                                             <span>{{ $r }}</span>
                                         </li>
-                                        @endif
-                                        @endif
+
                                         @endforeach
                                     </ul>
                                 </div>
