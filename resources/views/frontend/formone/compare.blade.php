14d891a3753731e68fe119b5f029b3da667bb249
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-09-23 17:52:58 +0500

    Update compare.blade.php

--- resources/views/frontend/formone/compare.blade.php
+++ resources/views/frontend/formone/compare.blade.php
@@ -21,13 +21,93 @@
        </div>
     </div>
 </section>
-
-
+<style>
+   .comparerow{
+      display: flex;
+   }
+   .comparebox{
+      @if(DB::table('compare_plans')->where('comparenumber'  ,$id)->count() == 3)
+         width: 33%;
+      @else
+         width: 48%;
+      @endif
+      margin: 5px;
+   }
+   .compareimagelogo{
+      text-align: center;
+   }
+   .coverageanddeductibles{
+      display: flex;
+   }
+   .coverageammount{
+      width: 50%;
+   }
+   .deductibles{
+      width: 50%;
+      text-align: right;
+   }
+   .coverageanddeductibles h2{
+      font-size: 18px;
+   }
+   .comparebox .card{
+      box-shadow: 5px 5px 12px rgb(87 106 134 / 20%);
+      border: none;
+      border-radius: 10px;
+   }
+   .card-plan__pricing-row {
+       padding-top: 20px;
+       width: 100%;
+       display: flex;
+       justify-content: space-between;
+   }
+   .price-value {
+       font-size: 32px;
+       line-height: 40px;
+       font-weight: 700;
+       color: #2b3481;
+   }
+   .button{
+      border-radius: 20px;
+       height: 40px;
+       font-size: 14px;
+       font-weight: 700;
+       line-height: 18px;
+       color: #2b3481;
+       border: 0;
+       transition: .3s ease-in-out;
+       outline: none;
+       display: inline-flex;
+       align-items: center;
+       justify-content: center;
+       padding: 10px 40px;
+       color: #fff;
+       background-color: #5ea047;
+   }
+   .headingcard{
+      font-size: 20px;
+       line-height: 28px;
+       margin-bottom: 3px;
+       margin-top: 3px;
+       color: #12b48b;
+       font-weight: 700;
+   }
+   #accordion .card{
+      background-color: #fff;
+      border-radius: 1rem;
+      box-shadow: 0 10px 24px rgba(109,126,152,.08);
+      border: none;
+   }
+   .card-header{
+      background-color: white;
+      border: none;
+   }
+</style>
+{{ DB::table('compare_plans')->where('comparenumber'  ,$id)->count() }}
 <section class="card-slide ah-slider-setting">
-   <div class="container-homepage">
-   <div class="wrapper">
+   <div class="container">
       <!-- Контент -->
-      <div class="row">
+
+      <div class="comparerow">
          @foreach(DB::table('compare_plans')->where('comparenumber'  ,$id)->get() as $r)
          @php
             $plan = DB::table('wp_dh_insurance_plans')->where('id' , $r->plan_id)->first();
@@ -35,61 +115,36 @@
             $insurance_company = $plan->insurance_company;
             $company = DB::table('wp_dh_companies')->where('comp_id' , $insurance_company)->first();
          @endphp
-         <div class="slider__item col-md-4">
-            <div class="card slider-card border-0">
-               <div class="card-body text-center">
-                  <div class="simple-online-transparent-slider">
-                       <img  style="width:180px !important;height:80px;" src="{{ url('public/images') }}/{{ $company->comp_logo }}">
+            <div class="comparebox">
+                  <div class="card">
+                        <div class="card-body">
+                              <div class="compareimagelogo">
+                                 <img  style="width:180px !important;height:80px;" src="{{ url('public/images') }}/{{ $company->comp_logo }}">
+                              </div>
+                              <div class="coverageanddeductibles mt-3">
+                                 <div class="coverageammount">
+                                    <h2>Coverage : ${{ $r->coverage_ammount }}</h2>
+                                 </div>
+                                 <div class="deductibles">
+                                    <h2>Deductible : ${{ $r->deductibles }}</h2>
+                                 </div>
+                              </div>
+                              <div  class="card-plan__pricing-row">
+                                 <div  class="plan-price subheading-2">
+                                    <span class="price-value">${{ number_format($r->price,2) }}</span>
+                                 </div>
+                                 <div class="plan-card-cta-container">
+                                    <a href="" class="button button-secondary">
+                                       <span >Buy</span><!---->
+                                    </a>
+                                 </div>
+                              </div>
+                        </div>
                   </div>
-                  <div class="slider-heading">
-                     <h2>Coverage : ${{ $r->coverage_ammount }}</h2>
-                  </div>
-                  <div class="slider-pargraph">
-                     <p>$ <?php echo number_format($r->price,2); ?></p>
-                  </div>
-               </div>
             </div>
-         </div>
          @endforeach
       </div>
-   </div>
-   <script type="text/javascript">
-      $(document).ready(function() {
-        $(".slider").slick({
-          arrows: true,
-          nextArrow: '<button class="slickarrowsnext"><i class="fa fa-solid fa-arrow-right  slick-next"></i></button>',
-          prevArrow: '<button class="slickarrowsprevious"><i class="fa fa-solid fa-arrow-left slick-prev"></i></button>',
-          dots: false,
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          autoplay: false,
-          speed: 900,
-          autoplaySpeed: 700,
-          responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2,
-                  slidesToScroll: 1,
-              }
-            },
-            {
-              breakpoint: 550,
-              settings: {
-                slidesToShow: 1,
-                  slidesToScroll: 1,
-              }
-            }
-          ]
-        });
-      });
-
-   </script>
 </section>
-
-
-
-
    <div  class="quote-compare__template container-homepage">
       <section class="quote-compare__spec-content" >
          <div class="grid-container">
@@ -104,63 +159,40 @@
                      <div class="card">
                          <div class="card-header" id="headingOne">
                              <h5 class="mb-0">
-                                 <button class="btn btn-link" data-toggle="collapse" data-target="#collapse{{ $r->id }}" aria-expanded="true" aria-controls="collapse{{ $r->id }}">
+                                 <span class="btn headingcard" data-toggle="collapse" data-target="#collapse{{ $r->id }}" aria-expanded="true" aria-controls="collapse{{ $r->id }}">
                                      <img  src="{{ url('public/images') }}/{{ $r->icon }}" alt="Overview">
                                      {{ $r->name }}
-                                 </button>
+                                 </span>
                              </h5>
                          </div>
 
                          <div id="collapse{{ $r->id }}" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                              <div class="card-body">
-                                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                 @foreach(DB::table('wp_dh_insurance_plans_benefits')->where('benifitcategory' , $r->id)->get() as $b)
+                                 <div class="panel-content__table-row">
+                                    <div  class="panel-content__table-cell panel-content-subheading">
+                                       <span  class="panel-content--heading--container">
+                                          <div class="panel-content--heading">{{ $b->benefits_head }}</div>
+                                       </span>
+                                    </div>
+                                    @foreach(DB::table('compare_plans')->where('comparenumber'  ,$id)->get() as $h)
+                                    <?php
+                                          $plan = DB::table('wp_dh_insurance_plans')->where('id' , $h->plan_id)->first();
+                                          $planname = $plan->plan_name;
+                                       ?>
+
+                                    <div class="panel-content__table-cell">
+                                       <div id="fw-500" class="text-content">@if(DB::table('wp_dh_insurance_plans_benefits')->where('plan_id' , $h->plan_id)->where('benefits_head' , $b->benefits_head)->first()){{ DB::table('wp_dh_insurance_plans_benefits')->where('plan_id' , $h->plan_id)->where('benefits_head' , $b->benefits_head)->first()->benefits_desc }} @else N/A @endif</div>
+                                    </div>
+                                    @endforeach
+                                 </div>
+                                 @endforeach
                              </div>
                          </div>
                      </div>
                      @endforeach
                  </div>
              </div>
-            <div  class="grid-row">
-               <div  class="grid-col-12">
-                  <div  class="spec-content__expansion-panels">
-                     @foreach(DB::table('plan_benifits_categories')->orderby('order' , 'desc')->get() as $r)
-                     <div onclick="showbenifitpanel({{$r->id}})" id="benefit-panel-{{ $r->id }}" class="expansion-panel mb-3">
-                        <div  class="expansion-panel__header">
-                           <div class="panel-header-icon">
-                               <img  src="{{ url('public/images') }}/{{ $r->icon }}" alt="Overview">
-                           </div>
-                           <div class="panel-header-text">
-                              <h3 class="header-title heading-4">{{ $r->name }}</h3>
-                              <!-- <h4 class="header-subtitle body-text-2 text-secondary-color"> {{ $r->description }} </h4> -->
-                           </div>
-                           <i  class="icon icon-chevron-down"></i>
-                        </div>
-                        <div class="expansion-panel__content" id="benifitpaneltoshow{{ $r->id }}">
-                           @foreach(DB::table('wp_dh_insurance_plans_benefits')->where('benifitcategory' , $r->id)->get() as $b)
-                           <div class="panel-content__table-row">
-                              <div  class="panel-content__table-cell panel-content-subheading">
-                                 <span  class="panel-content--heading--container">
-                                    <div class="panel-content--heading">{{ $b->benefits_head }}</div>
-                                 </span>
-                              </div>
-                              @foreach(DB::table('compare_plans')->where('comparenumber'  ,$id)->get() as $h)
-                              <?php
-                                    $plan = DB::table('wp_dh_insurance_plans')->where('id' , $h->plan_id)->first();
-                                    $planname = $plan->plan_name;
-                                 ?>
-
-                              <div class="panel-content__table-cell">
-                                 <div id="fw-500" class="text-content">@if(DB::table('wp_dh_insurance_plans_benefits')->where('plan_id' , $h->plan_id)->where('benefits_head' , $b->benefits_head)->first()){{ DB::table('wp_dh_insurance_plans_benefits')->where('plan_id' , $h->plan_id)->where('benefits_head' , $b->benefits_head)->first()->benefits_desc }} @else N/A @endif</div>
-                              </div>
-                              @endforeach
-                           </div>
-                           @endforeach
-                        </div>
-                     </div>
-                     @endforeach
-                  </div>
-               </div>
-            </div>
          </div>
       </section>
       <section class="quote-compare__disclaimer ah_compare__disclaimer" style="padding-top:0;" >
