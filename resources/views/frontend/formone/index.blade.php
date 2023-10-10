384456aa50cec9048d934fe850aafd44c56e5ef8
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-10-10 16:54:11 +0500

    asdsadsad

--- resources/views/frontend/formone/index.blade.php
+++ resources/views/frontend/formone/index.blade.php
@@ -29,43 +29,63 @@
 <!-- <link rel="stylesheet" type="text/css" href="{{ url('public/front/css/bootstraptwo.min.css')}}"> -->
 <link rel="stylesheet" type="text/css" href="{{ url('public/front/css/essentials.css')}}">
 <link rel="stylesheet" type="text/css" href="{{ url('public/front/css/tab_style.css') }}">
-<section class="tabshead">
-	<div class="container">
-		<div class="row tabs">
-			<div class="col-md-4 col-xs-4 text-center information_qoutes">
-				<button class="btn active">
-					<i class="fa fa-user"></i> Information
-				</button>
-			</div>
-			<div class="col-md-4 col-xs-4 text-center price_qoutes">
-				<button class="btn ">
-					<i class="fa fa-shopping-cart"></i> Quotes
-				</button>
-			</div>
-			<div class="col-md-4 col-xs-4 text-center apply_qoutes">
-				<button class="btn ">
-					<i class="fa fa-edit"></i> Apply / Buy
-				</button>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="tabscontent">
+<section class="tabscontent" style="padding-top: 90px !important;">
 	@if($fields['form_layout'] == 'layout_1')
-		@include('frontend.formone.includes.formlayoutone')
+		@include('frontend.travelinsurance.includes.form-one-supervisa')
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <div style="background-color:#f4f7fa;padding-bottom: 30px;" class="container-homepage">
+            <div class="quotationscards">
+
+            </div>
+        </div>
 	@elseif($fields['form_layout'] == 'layout_2')
-		@include('frontend.formone.includes.formlayouttwo')
+		@include('frontend.travelinsurance.includes.form-two-supervisa')
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <div style="background-color:#f4f7fa;padding-bottom: 30px;" class="container-homepage">
+            <div class="quotationscards">
+
+            </div>
+        </div>
 	@elseif($fields['form_layout'] == 'layout_3')
 		@include('frontend.formone.includes.formlayoutthree')
 	@elseif($fields['form_layout'] == 'layout_4')
-		@include('frontend.formone.includes.formlayoutfour')
+		@include('frontend.travelinsurance.includes.form-four-supervisa')
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <div style="background-color:#f4f7fa;padding-bottom: 30px;" class="container-homepage">
+            <div class="quotationscards">
+
+            </div>
+        </div>
 	@elseif($fields['form_layout'] == 'layout_5')
-		@include('frontend.formone.includes.formlayoutfive')
+		@include('frontend.travelinsurance.includes.form-five-supervisa')
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <div style="background-color:#f4f7fa;padding-bottom: 30px;" class="container-homepage">
+            <div class="quotationscards">
+
+            </div>
+        </div>
 	@elseif($fields['form_layout'] == 'layout_6')
-		@include('frontend.formone.includes.formlayoutsix')
+		@include('frontend.travelinsurance.includes.form-six-supervisa')
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <div style="background-color:#f4f7fa;padding-bottom: 30px;" class="container-homepage">
+            <div class="quotationscards">
+
+            </div>
+        </div>
 	@elseif($fields['form_layout'] == 'layout_7')
-		@include('frontend.formone.includes.formlayoutseven')
+		@include('frontend.travelinsurance.includes.form-seven-supervisa')
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <div style="background-color:#f4f7fa;padding-bottom: 30px;" class="container-homepage">
+            <div class="quotationscards">
+
+            </div>
+        </div>
 	@endif
 </section>
 @endsection
